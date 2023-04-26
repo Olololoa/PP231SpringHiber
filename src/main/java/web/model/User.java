b320a09c66093605111a6aa7534685029515bf08
Olololoa <124029765+Olololoa@users.noreply.github.com>
2023-04-27 01:29:21 +0300

    Add files via upload

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,62 @@
+package web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Setter
+    @Getter
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Getter
+    @Setter
+    private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    @Getter
+    @Setter
+    private String email;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age='" + age + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
